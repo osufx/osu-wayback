37ade4fd34d87ca2cb30733bc438c19d394afdaa
Sunpy <jonas_steinsvik@hotmail.no>
2018-01-13 21:39:01 +0100

    Actual some sort of navigation system

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 import json
 import MySQLdb
 import MySQLdb.cursors
-from flask import Flask, make_response, request, render_template
+from flask import Flask, make_response, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -9,8 +9,13 @@ with open("config.json", "r") as f:
     config = json.load(f)
 
 @app.route("/")
+@app.route("/home")
 def home_index():
     return render_template("index.html")
+
+@app.route("/download")
+def download_index():
+    return render_template("download.html")
 
 if __name__ == "__main__":
     app.run(**config["web"])
