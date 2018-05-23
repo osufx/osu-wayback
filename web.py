1617b60ea6e6637f684616c04924becc8f394846
Sunpy <sunpy@osufx.com>
2018-05-23 22:59:21 +0200

    Added api endpoint

--- web.py
+++ web.py
@@ -2,20 +2,29 @@ import json
 import MySQLdb
 import MySQLdb.cursors
 from flask import Flask, make_response, request, render_template, jsonify
+from handlers import update
 
 app = Flask(__name__)
 
 with open("config.json", "r") as f:
-    config = json.load(f)
+	config = json.load(f)
 
 @app.route("/")
 @app.route("/home")
 def home_index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/download")
 def download_index():
-    return render_template("download.html")
+	return render_template("download.html")
+
+@app.route("/api")
+def api_index():
+	return render_template("api.html")
+
+@app.route("/api/getUpdate", methods=["GET", "POST"])
+def api_update():
+	return update.handle(request)
 
 if __name__ == "__main__":
-    app.run(**config["web"])
+	app.run(**config["web"])
