807c06504ff7e65b7d58176279adb653b99b0207
Sunpy <sunpy@osufx.com>
2018-05-31 11:03:06 +0200

    Pass allowed arguments into missing args error

--- handlers/getfile.py
+++ handlers/getfile.py
@@ -4,7 +4,10 @@ allowed_args = ["file_hash", "file_version", "timestamp"]
 
 def handle(request):
     if len([x for x in request.args if x in allowed_args]) == 0:
-        return {"error": "Missing valid args"}
+        return {
+                "error": "Missing valid args",
+                "allowed": allowed_args
+            }
 
     for i in range(len(allowed_args)): # Gets the first valid argument and sets it as the method handler
         method = request.args.get(allowed_args[i])
